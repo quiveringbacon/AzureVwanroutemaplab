149e19a2fc52d0e11251beea46d5aafaf7bd98ca
quiveringbacon <128983862+quiveringbacon@users.noreply.github.com>
2024-08-23 13:56:08 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -566,7 +566,7 @@ resource "azurerm_public_ip" "spoke1vm-pip" {
   name                = "spoke1vm-pip"
   location            = azurerm_resource_group.RG.location
   resource_group_name = azurerm_resource_group.RG.name
-  allocation_method = "Dynamic"
+  allocation_method = "Static"
   timeouts {
     create = "2h"
     read = "2h"
@@ -579,7 +579,7 @@ resource "azurerm_public_ip" "spoke2vm-pip" {
   name                = "spoke2vm-pip"
   location            = azurerm_resource_group.RG.location
   resource_group_name = azurerm_resource_group.RG.name
-  allocation_method = "Dynamic"
+  allocation_method = "Static"
   timeouts {
     create = "2h"
     read = "2h"
@@ -593,7 +593,7 @@ resource "azurerm_public_ip" "onpremvm-pip" {
   name                = "onpremvm-pip"
   location            = azurerm_resource_group.RG.location
   resource_group_name = azurerm_resource_group.RG.name
-  allocation_method = "Dynamic"
+  allocation_method = "Static"
   timeouts {
     create = "2h"
     read = "2h"
